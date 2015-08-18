f7d192d2c3e3b98012aeb85a37decf1f0cf97d8a
Marcus Ottosson <konstruktion@gmail.com>
2015-08-18 16:29:19 +0100

    Adding data to Context

--- pyblish_standalone/plugins/collect_kwargs.py
+++ pyblish_standalone/plugins/collect_kwargs.py
@@ -10,8 +10,11 @@ class CollectKwargs(pyblish.api.Collector):
     def process(self, context):
         kwargs = pyblish_standalone.kwargs.__dict__.copy()
 
-        self.log.info("Converting kwargs from tuple to dict")
-        kwargs["data"] = dict(kwargs.pop("data"))
-
         self.log.info("Storing kwargs: %s" % kwargs)
         context.set_data("kwargs", kwargs)
+
+        self.log.info("Adding data from command-line into Context..")
+        data = dict(kwargs.pop("data", list()))
+        for key, value in data.iteritems():
+            self.log.info("%s = %s" % (key, value))
+            context.set_data(key, value)
