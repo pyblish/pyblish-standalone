a20378e5fb3f47a80b170e3d0cfe6352748c0d70
Marcus Ottosson <konstruktion@gmail.com>
2015-08-18 16:22:32 +0100

    Automatically shutting down on terminal session end

--- pyblish_standalone/executable.py
+++ pyblish_standalone/executable.py
@@ -1,24 +1,30 @@
-import sys
 import time
 import socket
 
-import pyblish.api
-import pyblish_integration
+import pyblish_integration.lib
 
 
-def main():
-    # launching pyblish ui
+def start():
+    """Start Pyblish QML"""
     pyblish_integration.setup()
 
     max_tries = 5
     while True:
-      try:
-        time.sleep(0.5)
-        pyblish_integration.show()
-      except socket.error as e:
-        if not max_tries:
-          raise Exception("Couldn't run Pyblish QML: %s" % e)
+        try:
+            time.sleep(0.5)
+            pyblish_integration.show()
+        except socket.error as e:
+            if max_tries <= 0:
+                raise Exception("Couldn't run Pyblish QML: %s" % e)
+            else:
+                print("%s tries left.." % max_tries)
+                max_tries -= 1
         else:
-          max_tries -= 1
-      else:
-          break
+            break
+
+    print("Launching Pyblish..")
+
+
+def stop():
+    """Hide Pyblish QML"""
+    pyblish_integration.lib.proxy.hide()
