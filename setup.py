4cd17111f4ee8085854947771529607c3aa700c3
Toke Jepsen <tokejepsen@gmail.com>
2016-08-04 16:37:12 +0100

    setup.py wip

--- setup.py
+++ setup.py
@@ -44,4 +44,8 @@ setup(
     zip_safe=False,
     classifiers=classifiers,
     package_data={"pyblish_standalone": ["plugins/*.py"]},
+    install_requires=[
+        "pyblish-base>=1.4",
+        "pyblish-qml>=0.7.0"
+    ],
 )
